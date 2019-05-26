13f0343c8ac12463d0a6b8fbf9100b8a572a44bb
Yıldız Uysal <merveyildizuysal@gmail.com>
2019-05-26 18:56:18 +0300

    first add mapkit

--- Travel Map Book/ViewController.swift
+++ Travel Map Book/ViewController.swift
@@ -7,19 +7,18 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate  {
 
+    @IBOutlet weak var myMap: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myMap.delegate = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
+    
 
 }
 
