f743c2bcf613f549609e4b1d6da10b1deecde623
Yıldız Uysal <merveyildizuysal@gmail.com>
2019-05-30 01:23:51 +0300

    This commit; find the your location when in use

--- Travel Map Book/ViewController.swift
+++ Travel Map Book/ViewController.swift
@@ -8,16 +8,31 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController, MKMapViewDelegate  {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate  {
 
     @IBOutlet weak var myMap: MKMapView!
+    var locationManager = CLLocationManager()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         myMap.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest //Çok pil harcar
+        locationManager.requestWhenInUseAuthorization() //gerekli kullanımda takip eder
+        locationManager.startUpdatingLocation() 
         
     }
     
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        myMap.setRegion(region, animated: true)
+    }
     
 
 }
